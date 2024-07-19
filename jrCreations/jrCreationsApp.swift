80435311bb1b5dc8195831006d7681648195637b
stevenTheDeve <steventhedeve@gmail.com>
2024-07-19 03:36:03 -0700

    Initial Commit

---
+++ jrCreations/jrCreationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  jrCreationsApp.swift
+//  jrCreations
+//
+//  Created by Estevan Hinojosa on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct jrCreationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
